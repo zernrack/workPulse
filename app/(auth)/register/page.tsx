ea24bc8fffb58592c77ab0076394a235c2837b82
Renz Anthony Rackhold <rackhold@gmail.com>
2025-06-24 17:52:13 +0800

    add auth pages

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,15 @@
+// app/register/page.tsx
+import { RegisterForm } from '@/components/register-form'
+import React from 'react'
+
+function RegisterPage() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-blue-600">
+          <div className="w-full max-w-sm">
+            <RegisterForm />
+          </div>
+        </div>
+  )
+}
+
+export default RegisterPage
