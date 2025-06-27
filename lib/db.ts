0700af7d51b6e60f0d81791a58f630e486026fe9
Renz Anthony Rackhold <rackhold@gmail.com>
2025-06-27 10:40:28 +0800

    create server actions for tasks

--- lib/db.ts
+++ lib/db.ts
@@ -2,5 +2,5 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "../db/schema";
 
-const client = postgres(process.env.DATABASE_URL!, {prepare: false})
-export const db = drizzle(client, { schema })
+const client = postgres(process.env.DATABASE_URL!, { prepare: false });
+export const db = drizzle(client, { schema });
